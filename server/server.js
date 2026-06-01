2325e3ddf5f95a23d5d9689bd349b2ba44b47cd0
KingDom987 <dombuyinglambo@gmail.com>
2026-06-01 11:37:57 +0200

    fix homepage

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Imperium Backend Running");
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
 /* REGISTER */
